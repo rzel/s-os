348d2007a51d323ad3c8b65670daa7eee310bb5d
stephen.13 <stephen.13@39fd31e7-933e-0410-a28a-1bbd0007c3db>
2007-11-16 12:58:41 +0000

    Update

--- kernel.cpp
+++ kernel.cpp
@@ -3,8 +3,10 @@
 int main(struct multiboot *mboot_ptr)
 {
 	video vid;
-	
 	vid.write("Welcome to SoS version 0.0.1");
+	
+	
+	//__cxa_finalize(0); - This will be required once globals are supported
 	
 	// Just to test if this works
 	return 0xDEADBEEF;
--- makefile
+++ makefile
@@ -5,6 +5,7 @@ all:
 	g++ -c video/*.cpp  -nostdlib -fno-builtin -fno-rtti -fno-exceptions
 	nasm -f elf kernel.asm -o kernel_asm.o
 	ld -T link.ld
+	rm *.o
 
 clean:
 	rm *.o
